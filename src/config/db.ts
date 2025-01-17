21e8a4a70a8623ae89d20021a7d09f5380c9fcf7
Leo en laptop <leonar2zb@gmail.com>
2025-01-17 17:54:05 -0300

    + Instalación y uso del paquete sequilize-typescript para usar en BD * Modificado db.ts para usar sequilize-typescript + Creado el primer modelo utilizando decoradores * Modificado tsconfig.ts para permitir decoradores

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
 import dotenv from 'dotenv'
 
 // Configurar dotenv para cargar variables de entorno 
@@ -6,7 +6,9 @@ dotenv.config();
 const strConecction = process.env.DATABASE_URL
 
 //para evitar error de conexión SSL, se debe anexar a la cadena "?ssl=true"
-const db = new Sequelize(strConecction)
+const db = new Sequelize(strConecction, {
+    models: [__dirname + '/../models/**/*.ts']
+})
 
 /*otra variante en lugar de anexar "?ssl=true" sería incluir las opciones de configuración, así:
 const db1 = new Sequelize(strConecction, {
---
+++ src/models/Product.model.ts
@@ -0,0 +1,27 @@
+import { CHAR } from 'sequelize'
+import { Table, Column, Model, DataType, Default } from 'sequelize-typescript'
+
+// https://sequelize.org/docs/v7/models/data-types/
+@Table({
+    tableName: 'products'
+})
+
+class Product extends Model {
+    @Column({
+        type: DataType.STRING(100)
+    }
+    )
+    name: string
+
+    @Column({
+        type: DataType.FLOAT(6, 2)
+    })
+    price: number
+
+    @Column({
+        type: DataType.BOOLEAN
+    })
+    availability: boolean
+}
+
+export default Product
