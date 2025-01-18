50e5fcd34748dadaca42b117e5cd67b8f1f25ffa
Leo en laptop <leonar2zb@gmail.com>
2025-01-18 10:30:26 -0300

    * Más validaciones

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -6,6 +6,18 @@ import { check, validationResult } from 'express-validator'
 export const createProduct = async (req: Request, resp: Response) => {
     //console.log(colors.bgBlue(req.body))
     await check('name').notEmpty().withMessage('El nombre del producto es obligatorio').run(req)
+    await check('price').isFloat({ gt: 0 }).withMessage('Sólo números positivos aquí').run(req)
+    /* se pueden usar validaciones personalizadas si fuera necesario como en esta
+    await check('price').custom(value => value > 0).withMessage('Sólo número mayor que cero').run(req)
+    
+    las validaciones se pueden concatenar una tras otra y cada una con su mensaje. Por legibilidad se puede separar en lineas ejemplo:
+    await check('name').isAlpha('es-ES').withMessage("Solo letras de español. No espacio ni otro")
+    .isLength({ min: 3, max: 10 }).withMessage("de 3 a 10 caracteres")
+    .isUppercase().withMessage("Solo en mayúsculas").run(req)
+
+    Existe gran número de validacione como para emails, etc además de poder usar expresiones regulares con matches
+    */
+
     let errors = validationResult(req)
     if (!errors.isEmpty()) {
         return resp.status(400).json({ errors: errors.array() })
