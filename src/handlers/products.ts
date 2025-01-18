28be6a6acba35a9ac6188d1341c6d028040f6afd
Leo en laptop <leonar2zb@gmail.com>
2025-01-17 21:46:14 -0300

    * Probando insertar un registro * Modificado tsconfig para instanciar objetos

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express'
 import colors from 'colors'
+import Product from '../models/Product.model'
 
-export const createProduct = (req: Request, resp: Response) => {
-    console.log(colors.bgBlue(req.body))
+export const createProduct = async (req: Request, resp: Response) => {
+    //console.log(colors.bgBlue(req.body))
+    const product = new Product(req.body)
+    product.save()
     resp.json(req.body)
 }
