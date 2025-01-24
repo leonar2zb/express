7a5141ee837a8619058490fbe892da06600e9fb4
Leo en laptop <leonar2zb@gmail.com>
2025-01-24 17:37:31 -0300

    + Habilitando CORS para permitir el frontend exclusivamente(o cliente REST)

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import swaggerUi from 'swagger-ui-express'
 import swaggerSpec, { swaggerUiOptions } from "./config/swagger";
 import router from "./router";
 import db from "./config/db";
+import cors, { CorsOptions } from "cors";
 import colors from 'colors'
 
 //Conectar a la BD
@@ -22,8 +23,20 @@ export async function connectDB() {
 }
 connectDB()
 
+// permitir conexiones CORS desde el frontend
+const CorsOptions: CorsOptions = {
+    origin: function (origin, callback) {
+        if ((origin === process.env.FRONTEND_URL) // desde la IP autorizada
+            || !origin) // o no tiene un origin(otro backend, cliente REST como Thunderclient o Postman para pruebas)
+            callback(null, true)
+        else
+            callback(new Error('Error origen de petición denegada'))
+    }
+}
+
 const server = express()
 
+server.use(cors(CorsOptions))
 // uso del middleware de express para interpretar JSON
 server.use(express.json())
 
